069bae631bd2839754e39a16992c821511e7cf98
Alexander <abeaumier@deloitte.com>
2024-03-27 11:34:42 -0700

    "post, put, delete"

--- src/app.js
+++ src/app.js
@@ -15,5 +15,21 @@ app.get("/restaurants/:id", async (req, res) => {
     res.json(restaurant);
 });
 
+app.post("/restaurants", async (req, res) => {
+    const restaurant = await Restaurant.create(req.body);
+    res.json(restaurant);
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+    const updatedRest = await Restaurant.update(req.body, { where: { id: req.params.id } });
+    res.json(updatedRest);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+    const deletedRest = await Restaurant.destroy({
+        where: { id: req.params.id },
+    });
+    res.json(deletedRest);
+});
 
 module.exports = app;
